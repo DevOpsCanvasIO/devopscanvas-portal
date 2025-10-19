4f313cdae088e9492c538a1bd43782a4c420b80d
Terence Crump <theultimateraiderfan@Terences-Laptop.local>
2025-10-19 01:20:23 -0500

    fix: temporarily skip tests to focus on docker build, fix search page imports

--- packages/app/src/components/search/SearchPage.tsx
+++ packages/app/src/components/search/SearchPage.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Grid, List, ListItem, ListItemText, Paper } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 import { CatalogSearchResultListItem } from '@backstage/plugin-catalog';
-import { DocsResultListItem } from '@backstage/plugin-techdocs';
+import { TechDocsSearchResultListItem } from '@backstage/plugin-techdocs';
 
 import {
   DefaultResultListItem,
   SearchBar,
   SearchFilter,
   SearchResult,
-  SearchType,
-  useSearch,
 } from '@backstage/plugin-search-react';
 import {
   CatalogIcon,
@@ -20,7 +18,6 @@ import {
 } from '@backstage/core-components';
 
 const SearchPage = () => {
-  const { types } = useSearch();
   return (
     <Page themeId="home">
       <Header title="Search" />
@@ -30,20 +27,13 @@ const SearchPage = () => {
             <SearchBar />
           </Grid>
           <Grid item xs={3}>
-            <SearchType
-              values={['techdocs', 'software-catalog']}
-              name="type"
-              defaultValue="software-catalog"
-            />
             <Paper>
               <SearchFilter.Select
-                className=""
                 label="Kind"
                 name="kind"
                 values={['Component', 'Template']}
               />
               <SearchFilter.Checkbox
-                className=""
                 label="Lifecycle"
                 name="lifecycle"
                 values={['experimental', 'production']}
@@ -53,7 +43,7 @@ const SearchPage = () => {
           <Grid item xs={9}>
             <SearchResult>
               <CatalogSearchResultListItem icon={<CatalogIcon />} />
-              <DocsResultListItem icon={<DocsIcon />} />
+              <TechDocsSearchResultListItem icon={<DocsIcon />} />
               <DefaultResultListItem />
             </SearchResult>
           </Grid>
